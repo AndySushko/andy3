55d1552cc99e791f35b3825b15e0a289528d71e3
AndySushko <130568845+AndySushko@users.noreply.github.com>
2024-04-11 13:58:38 +0300

    Update index.php

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ if ($errors) {
 }
 
 $user = 'u67381';
-$pass = '8515451';
+$pass = '';
 $db = new PDO('mysql:host=localhost;dbname=u67381', $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
